8a0d181d8627c9658d712a0080faa7cb1e01f6b8
Awesa <awesaatyanta.p@gmail.com>
2022-11-18 19:58:26 +0700

    fix: indentation on sample code

--- public/js/script.js
+++ public/js/script.js
@@ -4,7 +4,7 @@ const outputTextArea = document.getElementById("output-text-area");
 const runCodeButton = document.getElementById("run-code-button");
 
 codingTextArea.innerHTML =
-	'baca n\nulangin i dari 1 sampe n\n\tkalo i % 3 == 0 dan i % 5 == 0\n\t\ttulis "FizzBuzz"\n\t\tkalogak i % 3 == 0\n\t\t\ttulis "Fizz"\n\t\tkalogak i % 5 == 0\n\t\t\ttulis "Buzz"\n\t\tlainnya\n\t\ttulis i\n\t\tyaudah\nyaudah';
+	'baca n\nulangin i dari 1 sampe n\n\tkalo i % 3 == 0 dan i % 5 == 0\n\t\ttulis "FizzBuzz"\n\tkalogak i % 3 == 0\n\t\ttulis "Fizz"\n\tkalogak i % 5 == 0\n\t\ttulis "Buzz"\n\tlainnya\n\t\ttulis i\n\tyaudah\nyaudah';
 
 inputTextArea.innerHTML = 15;
 
